4c36148130ec6990a0a196c46fdf8a9db6a3eb2e
Churchill Aboge <tchillo@gmail.com>
2020-08-28 09:37:31 +0300

    Change render based on timestamp availability

--- lib/remote_backend_web/views/user_view.ex
+++ lib/remote_backend_web/views/user_view.ex
@@ -1,7 +1,11 @@
 defmodule RemoteBackendWeb.UserView do
   use RemoteBackendWeb, :view
 
+  def render(_conn, %{users: users, timestamp: timestamp}) when is_nil(timestamp) do
+    %{users: [], timestamp: timestamp}
+  end
+
   def render(_conn, %{users: users, timestamp: timestamp}) do
-    %{"users" => users, timestamp: timestamp}
+    %{users: users, timestamp: timestamp}
   end
 end
